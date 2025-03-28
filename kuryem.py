6338edf65817d2fa204a49b253e349b738deefe3
Emre Dinç <emre.dnc2002@gmail.com>
2025-03-28 04:39:25 +0300

    🎉 İlk commit: Kuryem uygulaması ve Flask sunucu premium entegrasyonu

--- kuryem.py
+++ kuryem.py
@@ -76,4 +76,4 @@ def home():
     return "🚀 Kuryem API çalışıyor!"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
